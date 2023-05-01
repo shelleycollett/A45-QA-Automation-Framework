6b9da87f77c95fda00fcdbe359b5c8e584f06ee4
khaledoni01 <khaledoni01@gmail.com>
2023-05-01 10:19:26 -0400

    refactored-session

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import java.time.Duration;
@@ -12,6 +13,7 @@ import java.util.UUID;
 public class BaseTest {
 
     public static WebDriver driver = null;
+    public static WebDriverWait wait = null;
 
     public static String url = "";
 
@@ -38,7 +40,8 @@ public class BaseTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         url = BaseURL;
         navigateToPage();
     }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,9 +1,52 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class LoginTests extends BaseTest {
+
+    Actions actions = new Actions(driver);
+
+    @Test public void actionMethods() {
+
+    }
+    // double click
+    public void doubleClickChoosePlaylist() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        // double click
+    }
+
+
+    // context click
+    public void chooseAllSongsList() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li a.songs")));
+    }
+    public void contextClickFirstSong(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item:nth-child(1)")));
+    }
+
+    // hover
+    public void hoverPlay() {
+        WebElement play = driver.findElement(By.cssSelector("[data-testid='play-btn]"));
+        // move to element
+    }
+
+    @Test
+    public void listOfSongsWebElements() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        //add assertion
+    }
+//    public List displayAllSongs() {
+//        // "#playlistWrapper td.title"
+//        add findElements step
+//        return songList;
+//    }
 
 
     @Test (dataProvider = "IncorrectLoginData", dataProviderClass = BaseTest.class, enabled = true, priority = 0, description = "Login with invalid email and valid password")
